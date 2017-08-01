8c1e71679a0e523b1b191e0e7f905749aa55b536
Frederik Ring <frederik.ring@gmail.com>
2017-08-01 20:22:39 +0200

    use default error strings for http errors

--- webhook.go
+++ webhook.go
@@ -15,8 +15,7 @@ func SetupWebhook(verifyToken string) (http.HandlerFunc, <-chan Update) {
 	handler := func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case http.MethodGet:
-			token := r.URL.Query().Get("hub.verify_token")
-			if token != verifyToken {
+			if token := r.URL.Query().Get("hub.verify_token"); token != verifyToken {
 				http.Error(w, "Verify token did not match", http.StatusUnauthorized)
 				return
 			}
@@ -27,17 +26,15 @@ func SetupWebhook(verifyToken string) (http.HandlerFunc, <-chan Update) {
 			}
 		case http.MethodPost:
 			bytes, bytesErr := ioutil.ReadAll(r.Body)
-			defer r.Body.Close()
 			if bytesErr != nil {
 				http.Error(w, bytesErr.Error(), http.StatusBadRequest)
 				return
 			}
+			defer r.Body.Close()
 			update := &UpdateRequest{}
-			unmarshalErr := json.Unmarshal(bytes, &update)
-			if unmarshalErr != nil {
-				out := Update{Error: unmarshalErr}
-				http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-				updates <- out
+			if unmarshalErr := json.Unmarshal(bytes, &update); unmarshalErr != nil {
+				http.Error(w, unmarshalErr.Error(), http.StatusInternalServerError)
+				updates <- Update{Error: unmarshalErr}
 				return
 			}
 			for _, entry := range *update.Entry {
@@ -47,7 +44,7 @@ func SetupWebhook(verifyToken string) (http.HandlerFunc, <-chan Update) {
 			}
 			io.WriteString(w, "OK")
 		default:
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
 		}
 	}
 	return handler, updates
